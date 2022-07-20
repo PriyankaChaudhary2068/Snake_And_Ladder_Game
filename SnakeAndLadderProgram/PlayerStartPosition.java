7a700486b53c28c9ea20a67c90942f474fb794ee
chutki12 <priyankachaudhary2068>
2022-07-20 22:32:57 +0530

    ADDED : Welcome To Snake And Ladder Program

---
+++ SnakeAndLadderProgram/PlayerStartPosition.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeAndLadderProblem;
+
+public class PlayerStartPosition {
+
+	public static void main(String[] args) {
+		 
+            
+		    int start = 0;
+			System.out.println("Starting Position:" +start);
+
+	}
+
+}
