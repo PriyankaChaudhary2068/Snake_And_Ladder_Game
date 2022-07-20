2a065f9e6002aa2fb48791e756983da30048ccf9
chutki12 <priyankachaudhary2068>
2022-07-21 00:43:41 +0530

    ADDED : This Code For Snake And Ladder Game

---
+++ SnakeAndLadderGame/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeAndLadderProblem;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snake And Ladder Game");
+
+	}
+
+}
